5885e819c41cb95d59d462b3b38d0a7e8fedb402
Viktor Siedov <viktorrabota22@gmail.com>
2018-08-09 11:30:11 +0300

    init project

---
+++ ARKitExample/ARKitExample/ARKitExample/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  ARKitExample
+//
+//  Created by Viktor Siedov on 09.08.2018.
+//  Copyright © 2018 Viktor Siedov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
