b0e19d29dd104b1bf76a6d78aff12339a320002b
Viktor Siedov <viktorrabota22@gmail.com>
2018-08-09 11:38:03 +0300

    pod init and install

---
+++ ARKitExample/ARKitExample/Pods/Target Support Files/Pods-ARKitExample/Pods-ARKitExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARKitExample : NSObject
+@end
+@implementation PodsDummy_Pods_ARKitExample
+@end
---
+++ ARKitExample/ARKitExample/Pods/Target Support Files/Pods-ARKitExampleTests/Pods-ARKitExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ARKitExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_ARKitExampleTests
+@end
