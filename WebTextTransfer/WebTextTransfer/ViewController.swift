65eaf778178c1817ca8b8fdb9e82bf0004953298
Viktor Siedov <viktorrabota22@gmail.com>
2018-09-02 20:15:29 +0300

    init example with websocket

---
+++ WebTextTransfer/WebTextTransfer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebTextTransfer
+//
+//  Created by Viktor Siedov on 02.09.2018.
+//  Copyright © 2018 Viktor Siedov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
